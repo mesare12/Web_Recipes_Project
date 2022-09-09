42d80bb806f440b362913655efba634a6ef18816
mesare12 <92524190+mesare12@users.noreply.github.com>
2022-09-09 09:45:42 +0200

    Css added

--- app.js
+++ app.js
@@ -1,14 +1,38 @@
-const http = require('http');
+const express = require('express');
+const app = express();
+const cors = require('cors')
+const db = require("./database.js")
 
-const hostname = '127.0.0.1';
+
+app.use(cors())
+app.use(express.static('public'))
+
+const bodyParser = require("body-parser")
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
-});
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+//start
+app.listen(port, () => 
+    console.log('Server running at ${port}'));
+
+
+
+
+app.get('/recipe', (req, res) => {
+    let sql = "select * from recipe"
+    let params = []
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+          res.status(400).json({"error":err.message});
+          return;
+        }
+        res.json({
+            "message":"success",
+            "recipe":rows
+        })
+      })
 });
